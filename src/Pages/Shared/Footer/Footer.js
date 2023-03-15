7264f5e115eec4f3921505106f102ed82e9b434c
shamolmojumder <shamolmojumder14@gmail.com>
2023-03-15 23:51:53 +0600

    71-9 Module Summary and set Footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,30 +1,36 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import footer from '../../../assets/images/footer.png'
 const Footer = () => {
-    return (
-        <footer className="footer p-10  text-neutral">
+  return (
+    <footer style={{background:`url(${footer})`,backgroundSize:"cover"}} className="p-10">
+      <div className="footer">
         <div>
-          <span className="footer-title">Services</span> 
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </div> 
-        <div>
-          <span className="footer-title">Company</span> 
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </div> 
-        <div>
-          <span className="footer-title">Legal</span> 
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <span className="footer-title">Services</span>
+          <Link to="/" className="link link-hover">Branding</Link>
+          <Link to="/" className="link link-hover">Design</Link>
+          <Link to="/" className="link link-hover">Marketing</Link>
+          <Link to="/" className="link link-hover">Advertisement</Link>
         </div>
-      </footer>
-    );
+        <div>
+          <span className="footer-title">Company</span>
+         <Link to="/"className="link link-hover">About us</Link>
+         <Link to="/"className="link link-hover">Contact</Link>
+         <Link to="/"className="link link-hover">Jobs</Link>
+         <Link to="/"className="link link-hover">Press kit</Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+         <Link to="/"className="link link-hover">Terms of use</Link>
+         <Link to="/"className="link link-hover">Privacy policy</Link>
+         <Link to="/"className="link link-hover">Cookie policy</Link>
+        </div>
+        </div>
+      <div className="text-center mt-32">
+        <p>{`Copyright © ${new Date().getFullYear()} - All right reserved by ACME Industries Ltd`}</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
