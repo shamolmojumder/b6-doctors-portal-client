03fe06a57200749ceceed36830ac461f65a3028b
shamolmojumder <shamolmojumder14@gmail.com>
2023-04-03 11:30:44 +0600

    73-2 Setup Login page, and Firebase Project setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const handleLogin=data=>{
+    console.log(data);
+  }
   //73-1 Module Introduction and react hook form setup 8:00
   return (
-    <div>
-      <div className="h-[800px] flex justify-center items-center">
-        <h2 className="text-4xl text-center">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center">Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
     
 
           <div className="form-control w-full max-w-xs">
@@ -23,12 +26,18 @@ const Login = () => {
               <span className="label-text">Password</span>
             </label>
             <input  type="password" className="input input-bordered w-full max-w-xs"  {...register("password")} placeholder="First name" />
+            <label className="label">
+              <span className="label-text underline">Forget Password</span>
+            </label>
           </div>
 
          
-          <p>{data}</p>
-          <input type="submit" />
+          {/* <p>{handleLogin.data}</p> */}
+          <input type="submit" className="btn btn-accent w-full" value="Login" />
         </form>
+        <p>New to Doctors Portal <Link className="text-secondary" to="/signup">Create new account</Link> </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">Continue with google</button>
       </div>
     </div>
   );
