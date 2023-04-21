0b93bee4f7dca456d8f502273974099d268abf39
shamolmojumder <shamolmojumder14@gmail.com>
2023-04-21 23:34:01 +0600

    73-6 (Recap) Create Auth Context and implement user sign up

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { AuthConext } from "../../contexts/AuthProvider";
 
 const SignUp = () => {
     const {register,handleSubmit,formState:{errors}}=useForm();
+    const {createUser}=useContext(AuthConext)
     const handleSignup=(data)=>{
         console.log(data);
+        createUser(data.email,data.password)
+        .then(result=>{
+          const user=result.user;
+          console.log(user);
+        })
+        .catch(error=>console.log(error))
     }
   return (
     <div className="h-[800px] flex justify-center items-center">
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,13 +1,21 @@
 import React, { createContext } from 'react';
-import getAuth from '/firebase/auth';
+// import getAuth from '/firebase/auth';
+
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+
 import app from '../firebase/firebase.config';
 
 export const AuthConext=createContext();
 const auth=getAuth(app);
 
+const createUser=(email,password)=>{
+    return createUserWithEmailAndPassword(auth, email, password)
+
+}
+
 const AuthProvider = ({children}) => {
     const authInfo={
-
+        createUser
     }
 
     return (
@@ -18,3 +26,5 @@ const AuthProvider = ({children}) => {
 };
 
 export default AuthProvider;
+
+//73-6 (Recap) Create Auth Context and implement user sign up 5:00
